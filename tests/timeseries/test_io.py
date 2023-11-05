cd4caed22770a6431f42aab4970e2a9c51f3b759
Bernhard Ryeng <bryeng@users.noreply.github.com>
2023-11-05 21:58:10 +0100

    updated tests

--- tests/timeseries/test_io.py
+++ tests/timeseries/test_io.py
@@ -1,19 +1,19 @@
 import os.path
 
 from timeseries import io
-from timeseries.properties import SeriesType as type
+from timeseries.properties import SeriesType
 
 
 def test_dataset_dirs() -> None:
-    dirs = io.DatasetDirectory(set_name="test-1", set_type=type("SIMPLE"))
+    dirs = io.DatasetDirectory(set_name="test-1", set_type=SeriesType(type="SIMPLE"))
     assert isinstance(dirs, io.DatasetDirectory)
 
 
 def test_dataset_datadir_path_as_expected() -> None:
     set_name = "test-2"
-    set_type = "SIMPLE"
+    set_type = SeriesType(type="SIMPLE")
     dirs = io.DatasetDirectory(set_name=set_name, set_type=set_type)
-    expected: str = f"{dirs.root_dir}/{set_name}"
+    expected: str = f"{dirs.type_path}/{set_name}"
     print(expected)
     print(dirs.data_dir)
     assert dirs.data_dir == expected
@@ -21,9 +21,9 @@ def test_dataset_datadir_path_as_expected() -> None:
 
 def test_dataset_metadir_path_as_expected() -> None:
     set_name = "test-3"
-    set_type = "simple"
+    set_type = SeriesType(type="SIMPLE")
     dirs = io.DatasetDirectory(set_name=set_name, set_type=set_type)
-    expected: str = f"{dirs.root_dir}/{set_name}"
+    expected: str = f"{dirs.type_path}/{set_name}"
     print(expected)
     print(dirs.metadata_dir)
     assert dirs.metadata_dir == expected
@@ -31,7 +31,7 @@ def test_dataset_metadir_path_as_expected() -> None:
 
 def test_dataset_directories_created_() -> None:
     set_name = "test-mkdir"
-    set_type = "simple"
+    set_type = SeriesType(type="SIMPLE")
     dirs = io.DatasetDirectory(set_name=set_name, set_type=set_type)
     dirs.makedirs()
     assert os.path.isdir(dirs.data_dir)
@@ -40,4 +40,4 @@ def test_dataset_directories_created_() -> None:
 
 def cleanup() -> None:
     pass
-    # os.path.del(f"{dirs.root_dir}/{set_type}/{set_name}")
+    # os.path.del(f"{dirs.type_path}/{set_type}/{set_name}")
