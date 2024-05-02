e2bafca1ad1d1d7113392f4a885c09e6ea2cb81f
Bernhard Ryeng <bryeng@users.noreply.github.com>
2024-05-02 11:26:23 +0200

    Improve tests for dataset resampling.

--- tests/test_dataset_time.py
+++ tests/test_dataset_time.py
@@ -1,7 +1,8 @@
-# type: ignore
-# ruff: noqa
+## ruff: noqa
 import logging
 import uuid
+
+import pytest
 
 from ssb_timeseries.dataset import Dataset
 
@@ -82,8 +83,32 @@ def test_dataset_groupby_mean(caplog):
     assert y.data.shape == (14, 3)
 
 
+@pytest.mark.skip(reason="Not ready yet.")
 @log_start_stop
-def test_dataset_resample(caplog):
+def test_dataset_groupby_auto(caplog):
+    caplog.set_level(logging.DEBUG)
+
+    x = Dataset(
+        name="test-groupby-auto", data_type=SeriesType.simple(), load_data=False
+    )
+
+    tag_values = [["p_pris", "q_pris", "r_pris", "p_volum", "q_volum", "r_volum"]]
+    x.data = create_df(
+        *tag_values, start_date="2022-01-01", end_date="2023-02-28", freq="D"
+    )
+    assert x.data.shape == (424, 7)
+    df = x.groupby("M", "auto")
+    df_mean = x.groupby("M", "mean")
+    df_sum = x.groupby("M", "sum")
+    ts_logger.warning(f"groupby:\n{df}")
+    # use of period index means 'valid_at' is not counted in columns
+    assert df.shape == (14, 6)
+    assert ~all(df == df_mean)
+    assert ~all(df == df_sum)
+
+
+@log_start_stop
+def test_dataset_resample_upsampling_ffil(caplog):
     caplog.set_level(logging.DEBUG)
 
     tag_values = [["p", "q", "r"]]
@@ -98,7 +123,7 @@ def test_dataset_resample(caplog):
     assert x.data.shape == (12, 4)
 
     y = x.resample("D", "ffill", closed="s")
-    ts_logger.warning(f"resample:\n{x.data}\n{y.name}\n{y.data}")
+    ts_logger.debug(f"resample:\n{x.data}\n{y.name}\n{y.data}")
     # beware of index column!
     # double check behaviour for lat period
     # verify / create test cases per Temporality
@@ -106,75 +131,20 @@ def test_dataset_resample(caplog):
     assert y.data.shape == (335, 3)
 
 
-# @log_start_stop
-# def test_dataset_groupby_sum(caplog):
-#     caplog.set_level(logging.DEBUG)
+@log_start_stop
+def test_dataset_resample_downsampling_w_mean(caplog):
+    caplog.set_level(logging.DEBUG)
 
-#     x = Dataset(name="test-groupby-sum", data_type=SeriesType.simple(), load_data=False)
-
-#     tag_values = [["p", "q", "r"]]
-#     x.data = create_df(
-#         *tag_values, start_date="2022-01-01", end_date="2023-02-28", freq="D"
-#     )
-#     assert x.data.shape == (424, 4)
-#     ts_logger.debug(f'groupby:\n{x.groupby("M", "sum")}')
-#     # use of period index means 'valid_at' is not counted in columns
-#     assert x.groupby("M", "sum").shape == (14, 3)
-
-
-# @log_start_stop
-# def test_dataset_groupby_mean(caplog):
-#     caplog.set_level(logging.DEBUG)
-
-#     x = Dataset(
-#         name="test-groupby-mean", data_type=SeriesType.simple(), load_data=False
-#     )
-
-#     tag_values = [["p", "q", "r"]]
-#     x.data = create_df(
-#         *tag_values, start_date="2022-01-01", end_date="2023-02-28", freq="D"
-#     )
-#     assert x.data.shape == (424, 4)
-#     df1 = x.groupby("M", "mean")
-#     ts_logger.warning(f"groupby:\n{df1}")
-#     # use of period index means 'valid_at' is not counted in columns
-#     assert df1.shape == (14, 3)
-
-
-# @log_start_stop
-# def test_dataset_groupby_auto(caplog):
-#     caplog.set_level(logging.DEBUG)
-
-#     x = Dataset(
-#         name="test-groupby-auto", data_type=SeriesType.simple(), load_data=False
-#     )
-
-#     tag_values = [["p_pris", "q_pris", "r_pris", "p_volum", "q_volum", "r_volum"]]
-#     x.data = create_df(
-#         *tag_values, start_date="2022-01-01", end_date="2023-02-28", freq="D"
-#     )
-#     assert x.data.shape == (424, 7)
-#     df = x.groupby("M", "auto")
-#     df_mean = x.groupby("M", "mean")
-#     df_sum = x.groupby("M", "sum")
-#     ts_logger.warning(f"groupby:\n{df}")
-#     # use of period index means 'valid_at' is not counted in columns
-#     assert df.shape == (14, 6)
-#     assert ~all(df == df_mean)
-#     assert ~all(df == df_sum)
-
-
-# @log_start_stop
-# def test_dataset_resample(caplog):
-#     caplog.set_level(logging.DEBUG)
-
-#     x = Dataset(name="test-resample", data_type=SeriesType.simple(), load_data=False)
-#     tag_values = [["p", "q", "r"]]
-#     x.data = create_df(
-#         *tag_values, start_date="2022-01-01", end_date="2022-12-31", freq="YS"
-#     )
-#     assert x.data.shape == (1, 4)
-#     # df = x.data.resample("M").mean()
-#     df = x.resample("M", "mean")
-#     ts_logger.warning(f"resample:\n{df}")
-#     # assert df.shape == (12, 3)
+    tag_values = [["p", "q", "r"]]
+    x = Dataset(
+        name="test-resample",
+        data_type=SeriesType.simple(),
+        load_data=False,
+        data=create_df(
+            *tag_values, start_date="2022-01-01", end_date="2022-12-31", freq="MS"
+        ),
+    )
+    assert x.data.shape == (12, 4)
+    y = x.resample("QE", "mean")
+    ts_logger.warning(f"resample:\n{x.data}\n{y.name}\n{y.data}")
+    assert y.data.shape == (4, 3)
